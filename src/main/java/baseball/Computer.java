3800e07c85eb6233d4f81991b47d334385a9e9d1
kmindev <kangkm0324@gmail.com>
2024-05-14 15:26:35 +0900

    feat: 난수 생성
    서로 다른 3자리인 난수를 생성한다.

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,26 @@
+package baseball;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class Computer {
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 9;
+
+    public List<Integer> generateNumber() {
+        List<Integer> nums = new ArrayList<>();
+
+        while (nums.size() != 3) {
+            int randomNum = pickNumberInRange(MIN_VALUE, MAX_VALUE); // n 부터 m 까지 랜덤 수 생성
+
+            if (!nums.contains(randomNum)) {
+                nums.add(randomNum);
+            }
+        }
+
+        return nums;
+    }
+}
