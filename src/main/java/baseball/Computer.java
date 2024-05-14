dc7416b236e6091082cb0106812c0840641bd7d9
kmindev <kangkm0324@gmail.com>
2024-05-14 18:38:58 +0900

    refactor: 볼 사이즈를 상수로 추출

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -7,13 +7,14 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Computer {
+    private static final int SIZE = 3;
     private static final int MIN_VALUE = 1;
     private static final int MAX_VALUE = 9;
 
     public List<Integer> generateNumber() {
         List<Integer> nums = new ArrayList<>();
 
-        while (nums.size() != 3) {
+        while (nums.size() != SIZE) {
             int randomNum = pickNumberInRange(MIN_VALUE, MAX_VALUE); // n 부터 m 까지 랜덤 수 생성
 
             if (!nums.contains(randomNum)) {
